e224d4b101e63b1a030946bb71a4245d1dbed10a
Nickolay Kuzmin <nickolay.kuzmin2001@gmail.com>
2022-07-21 17:17:46 +0400

    Step 3. Add a welcome_user function in brain_games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
+import prompt
+from brain_games.cli import welcome_user
 
 
 def main():
     print('Welcome to the Brain Games!')
+    user_name = prompt.string(prompt='May i have your name? ')
+    welcome_user(user_name)
+
 
 if __name__ == '__main__':
     main()
