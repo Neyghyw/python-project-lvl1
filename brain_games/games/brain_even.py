e6f8797d97504aa05ac307b1a95875500f2508a3
Nickolay Kuzmin <nickolay.kuzmin2001@gmail.com>
2022-07-26 13:22:24 +0400

    Step 9. Refactoring cli.py for hexlet test next try

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -4,7 +4,7 @@ import random
 def even_get_question_and_answer() -> (str, str):
     question = random.randint(1, 100)
     answer = (question % 2 == 0 and "yes") or "no"
-    return question, answer
+    return str(question), answer
 
 
 if __name__ == "__main__":
