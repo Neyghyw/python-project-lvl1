884016f2bb429a81475cd474ff01a55e6e5b23ae
Nickolay Kuzmin <nickolay.kuzmin2001@gmail.com>
2022-07-29 12:46:30 +0400

    Remove unsafe function eval in calc game. Add new function

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -4,10 +4,19 @@ import random
 RULES = 'What is the result of the expression?'
 
 
+def get_arithmetic_expression_result(operand1, operand2, operator):
+    if operator == "+":
+        return operand1 + operand2
+    elif operator == "-":
+        return operand1 - operand2
+    else:
+        return operand1 * operand2
+
+
 def get_question_and_answer() -> (str, str):
     operand1 = random.randint(1, 100)
     operand2 = random.randint(1, 100)
     operator = random.choice("+-*")
     question = f'{operand1} {operator} {operand2}'
-    answer = str(eval(question))
-    return question, answer
+    answer = get_arithmetic_expression_result(operand1, operand2, operator)
+    return question, str(answer)
