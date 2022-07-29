fd534fbfaa8c759f74d382f32a10b6081ba946a9
Nickolay Kuzmin <nickolay.kuzmin2001@gmail.com>
2022-07-29 10:47:25 +0400

    Rename game files

--- brain_games/games/game_data.py
+++ brain_games/games/game_data.py
@@ -1,23 +1,23 @@
-from brain_games.games import even_utils
-from brain_games.games import calc_utils
-from brain_games.games import prime_utils
-from brain_games.games import progression_utils
-from brain_games.games import gcd_utils
+from brain_games.games import even_game
+from brain_games.games import calc_game
+from brain_games.games import prime_game
+from brain_games.games import progression_game
+from brain_games.games import gcd_game
 
 
 question_and_answer = {
-    'brain-calc': calc_utils.get_calc_question_and_answer,
-    'brain-even': even_utils.get_even_question_and_answer,
-    'brain-gcd': gcd_utils.get_gcd_question_and_answer,
-    'brain-progression': progression_utils.get_progression_question_and_answer,
-    'brain-prime': prime_utils.get_prime_question_and_answer
+    'brain-calc': calc_game.get_calc_question_and_answer,
+    'brain-even': even_game.get_even_question_and_answer,
+    'brain-gcd': gcd_game.get_gcd_question_and_answer,
+    'brain-progression': progression_game.get_progression_question_and_answer,
+    'brain-prime': prime_game.get_prime_question_and_answer
 }
 
 
 rules = {
-    'brain-calc': calc_utils.RULES,
-    'brain-even': even_utils.RULES,
-    'brain-gcd': gcd_utils.RULES,
-    'brain-progression': progression_utils.RULES,
-    'brain-prime': prime_utils.RULES
+    'brain-calc': calc_game.RULES,
+    'brain-even': even_game.RULES,
+    'brain-gcd': gcd_game.RULES,
+    'brain-progression': progression_game.RULES,
+    'brain-prime': prime_game.RULES
 }
