d2d1592f3669dff949eb0422ac72618af62f755a
Nickolay Kuzmin <nickolay.kuzmin2001@gmail.com>
2022-07-29 13:08:10 +0400

    Refactoring

--- brain_games/games/__init__.py
+++ brain_games/games/__init__.py
@@ -1,4 +1,4 @@
-from brain_games.games import calc_game, prime_game, gcd_game, progression_game, even_game
+from brain_games.games import calc, prime, progression, gcd, even
 
 
-__all__ = ('calc_game', 'prime_game', 'gcd_game', 'progression_game', 'even_game')
+__all__ = ('calc', 'prime', 'gcd', 'progression', 'even')
