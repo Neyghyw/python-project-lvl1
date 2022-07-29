7f039cae7367722fe1a3993553f26e92362bf407
Nickolay Kuzmin <nickolay.kuzmin2001@gmail.com>
2022-07-29 13:02:36 +0400

    Refactoring

--- brain_games/games/__init__.py
+++ brain_games/games/__init__.py
@@ -1,1 +1,4 @@
+from brain_games.games import calc_game, prime_game, gcd_game, progression_game, even_game
+
+
 __all__ = ('calc_game', 'prime_game', 'gcd_game', 'progression_game', 'even_game')
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-from brain_games import game_engine, games
+from brain_games import engine, games
 
 
 def main():
-    game_engine.start_game_template(games.calc_game)
+    engine.start_game_template(games.calc_game)
 
 
 if __name__ == "__main__":
